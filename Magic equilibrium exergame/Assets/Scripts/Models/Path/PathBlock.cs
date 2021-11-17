d939e0800248cae175b359a3ce994233ea540321
fedeAlterio <48481385+fedeAlterio@users.noreply.github.com>
2021-11-17 16:36:41 +0100

    Path Generation started

---
+++ Magic equilibrium exergame/Assets/Scripts/Models/Path/PathBlock.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Models.Path
+{
+    public class PathBlock : MonoBehaviour
+    {
+        // Editor fields
+        [SerializeField] private Transform _entryTransform;
+        [SerializeField] private Transform _exitTransform;
+
+
+
+        // Properties
+        public Vector3 EntryPosition => _entryTransform.position;
+        public Vector3 ExitPosition => _exitTransform.position;
+    }
+}

4b72c17c3a5420436eb1d52b877b156fe96f9728
fedeAlterio <48481385+fedeAlterio@users.noreply.github.com>
2022-01-20 13:58:40 +0100

    Code cleanup

--- Magic equilibrium exergame/Assets/Scripts/Cameras/PerspectiveHandler.cs
+++ Magic equilibrium exergame/Assets/Scripts/Cameras/PerspectiveHandler.cs
@@ -19,20 +19,14 @@ namespace Assets.Scripts.Cameras
         // Initialization
         private void Start()
         {
-
+            var normalizedFloorToCameraClip = ComputeNormalizedFloorToCameraClip();
+            _customRendererTextureMaterial.SetMatrix("_FloorNormalizedToCameraClip", normalizedFloorToCameraClip);
         }
 
-        private void Update()
+        private Matrix4x4 ComputeNormalizedFloorToCameraClip()
         {
-            ComputeMatrix();
-            _customRendererTextureMaterial.SetMatrix("_FloorNormalizedToCameraClip", NormalizedFloorToCameraClip);
-        }
-
-
-        void ComputeMatrix()
-        {
-            var x = GetPerspectiveData();
-            NormalizedFloorToCameraClip = GetC(_cameras[1]) * GetA(x) * GetB(x);
+            var perspectiveData = GetPerspectiveData();
+            return GetC(_cameras[1]) * GetA(perspectiveData) * GetB(perspectiveData); 
         }
 
         private PerspectiveData GetPerspectiveData()
@@ -61,25 +55,8 @@ namespace Assets.Scripts.Cameras
         }
 
 
-        private void CreateTexture()
-        {            
-            var cameraTextures = _cameras.Select(x => ToTexture2D(x.targetTexture)).ToArray();
-            for(var i=0; i < FloorTexture.width; i++)
-                for(var j=0; j < FloorTexture.height; j++)
-                {
-                    var cameraPoint = ComputePointNew(new Vector2(i / (float) FloorTexture.width, j / (float) FloorTexture.height));
-                    var color = cameraTextures[cameraPoint.CameraId]
-                        .GetPixel(cameraPoint.CameraX, cameraPoint.CameraY);
-                    FloorTexture.SetPixel(i, j, color);
-                }
-            FloorTexture.Apply();
-        }
-
-
         // Properties
         private Camera FrontCamera => _cameras[0];
-        public Texture2D FloorTexture { get; private set; }
-        public Matrix4x4 NormalizedFloorToCameraClip { get; private set; }
 
 
         private Matrix4x4 GetA(PerspectiveData perspectiveData)
@@ -96,18 +73,13 @@ namespace Assets.Scripts.Cameras
         {
             var ux = perspectiveData.Ux;
             var uz = perspectiveData.Uz;
-            var c1 = new Vector4(ux.x, ux.y, -ux.z, 0);
+            var c1 = new Vector4(ux.x, ux.y, ux.z, 0);
             var c2 = perspectiveData.FloorWorldHeight* new Vector4(uz.x, uz.y, uz.z, 0);
             var c3 = new Vector4(0, 0, 1, 0);
             var c4 = new Vector4(0, 0, 0, 1);
             return new Matrix4x4(c1, c2, c3, c4);
         }
 
-        private Matrix4x4 TranslationMatrix(Vector3 v)
-        {
-            return Matrix4x4.Translate(v);
-        }
-         
         private Matrix4x4 GetC(Camera camera)
         {
             Matrix4x4 P = camera.projectionMatrix;
@@ -125,44 +97,6 @@ namespace Assets.Scripts.Cameras
             Vector3 ret = result;
             ret /= result.w;
             return ret;
-        }
-
-        private CameraPoint ComputePointNew(Vector2 pointFloorCoordinates)
-        {
-            var p = GetPerspectiveData();
-            var camera = _cameras[1];
-            var cameraWidthPixels = camera.targetTexture.width;
-            var cameraHeightPixels = camera.targetTexture.height;
-            var a = new Vector4(pointFloorCoordinates.x, pointFloorCoordinates.y, 0);
-            var point = Apply(NormalizedFloorToCameraClip, a);
-            if (IsInsideFrustum(point))
-            {
-                var x = (int)Mathf.Clamp(point.x * cameraWidthPixels, 0, cameraWidthPixels);
-                var y = (int)Mathf.Clamp(point.y * cameraHeightPixels, 0, cameraHeightPixels);
-                var cameraPoint = new CameraPoint(1, x, y);
-                return cameraPoint;
-            }
-            return new CameraPoint(0, 0, 0);
-        }
-
-
-        private bool IsInsideFrustum(Vector3 viewPortCoordinates)
-        {
-            return -0 <= viewPortCoordinates.x && viewPortCoordinates.x <= 1
-                && -0 <= viewPortCoordinates.y && viewPortCoordinates.y <= 1;
-        }
-
-
-
-        // Utils
-        Texture2D ToTexture2D(RenderTexture rTex)
-        {
-            Texture2D tex = new Texture2D(512, 512, TextureFormat.RGB24, false);
-            // ReadPixels looks at the active RenderTexture.
-            RenderTexture.active = rTex;
-            tex.ReadPixels(new Rect(0, 0, rTex.width, rTex.height), 0, 0);
-            tex.Apply();
-            return tex;
         }
     }
 }
